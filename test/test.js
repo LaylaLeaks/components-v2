dffd35dec319f0cb98e6f8dd4b813fcced36bc12
Layla <94177119+LaylaLeaks@users.noreply.github.com>
2026-03-15 23:36:34 +0100

    example

---
+++ test/test.js
@@ -0,0 +1,32 @@
+﻿const { Container, TextDisplay, Separator, Message, Sender } = require("components-v2");
+
+const sender = Sender.createBotSender({
+    botToken: "BOT-TOKEN",
+    channelId: "CHANNELID",
+    crosspost: false,
+});
+
+/* For more Container just add it (max. 10 Container possible) */
+const message = new Message()
+    .addContainer(
+        new Container()
+            .setAccentColor("7923c4")
+            .addTextDisplay("## Message Container")
+            .addSeparator({ divider: true, spacing: "small" })
+            .addTextDisplay("This is a Components v2 message!")
+            .addSeparator({ divider: true, spacing: "small" })
+            .addTextDisplay("-# This could be a cool footer")
+    )
+    .addContainer(
+        new Container()
+            .setAccentColor("7923c4")
+            .addTextDisplay("## Message Container")
+            .addSeparator({ divider: true, spacing: "small" })
+            .addTextDisplay("This is a Components v2 message!")
+            .addSeparator({ divider: true, spacing: "small" })
+            .addTextDisplay("-# This could be a cool footer")
+    );
+
+sender.send(message)
+    .then(() => console.log("Message sent!"))
+    .catch(console.error);
